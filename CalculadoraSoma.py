0ee93231ad1a13704f9e436ad867618036d2d1ca
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-03 19:09:06 +0200

    Updates

---
+++ CalculadoraSoma.py
@@ -0,0 +1,4 @@
+n1 = int(input('Me diga um valor: '))
+n2 = int(input('Me informe um segundo valor: '))
+s = n1 + n2
+print('A soma de {} com {} é {}'.format(n1, n2, s))
--- aula06a.py
+++ aula06a.py
@@ -4,3 +4,4 @@ s = n1 + n2
 print('A soma entre', n1, 'e' ,n2, 'vale', s)
 
 
+
