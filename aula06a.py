fc586c052852357cf6f137b5b79282efa0160c81
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-03 18:59:49 +0200

    Updates

---
+++ aula06a.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um valor'))      #Se usa o int antes para identificar o valor da variavel, e assim somar
+n2 = int(input('Digite o outro valor'))
+s = n1 + n2
+print('A soma entre', n1, 'e' ,n2, 'vale', s)
+
+
