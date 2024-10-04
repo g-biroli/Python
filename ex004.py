02e337be55ea8839db6481efbea7a27565d6aa07
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-04 17:37:04 +0200

    Aulas do curso

---
+++ ex004.py
@@ -0,0 +1,7 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espacos? ', a.isspace())
+print('É um numero? ', a.isnumeric())
+print('É alfabetico? ', a.isalpha())
+print('É um alfanumerico? ', a.isalnum())
+print('Está em maiusculas? ', a.isupper())
