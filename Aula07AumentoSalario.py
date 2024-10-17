433187443f14cdc30b3f127fe463af0c6f7fa9d6
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-17 18:52:09 +0200

    Aula07 Convertor de temperatura

---
+++ Aula07AumentoSalario.py
@@ -0,0 +1,12 @@
+#Exercicio de aumento de salário em 15%
+
+salario = float(input('Qual é o salário do funcionário? '))
+
+#novo = novo salario
+novo = salario + (salario * 15 /100)
+print('O salário sofreu um reajuste e seu salario de {}, com um aumento de 15%, passou a ser {}'.format(salario, novo))
+
+
+
+
+
