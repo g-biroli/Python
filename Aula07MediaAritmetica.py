3cef3496dc17c814298f02e2bcc39b63b7b46786
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-17 16:48:33 +0200

    Aula07 Media aritmetica

---
+++ Aula07MediaAritmetica.py
@@ -0,0 +1,7 @@
+#Média Aritmetica -> nota de faculdade
+
+n1 = float(input('Primeira Prova: '))
+n2 = float(input('Segunda Prova: '))
+média = (n1 + n2) / 2
+
+print('Amédia entre {} e {} é igual a {}'.format(n1, n2, média))
