4493c64d33d29922bcab1142ac1c2f401f54a6c6
Gabriel Biroli <gabrielbiroli@gmail.com>
2025-12-31 11:47:31 -0300

    Desafio Entrevista Python

--- CursoEmVideo/scriptAsimov.py
+++ CursoEmVideo/scriptAsimov.py
@@ -1,3 +1,8 @@
-print('Qual numero você está pensando? ')
-input()
-print('O valor pensado foi: ' + x)
+#Desafio de programa de Entrevista
+
+nome = input('Qual o seu nome? ')
+idade = int(input('Qual a sua idade? '))
+idade_futura = idade + 5
+
+print(f'Prazer {nome} eu sou o Gabriel, analista de dados da Mercedez, como posso te ajudar?')
+print(f'Interessante saber que você tem {idade} eu tenho uma idade um pouco mais abaixo, tenho 25 anos, daqui a 5 anos você terá {idade_futura} anos.')
