d964ed33b3e14fa8ce5598b6038f8312998f6df1
Gabriel Biroli <gabrielbiroli@gmail.com>
2026-01-01 10:36:58 -0300

    Primeiro commit 2026

--- CursoEmVideo/scriptAsimov.py
+++ CursoEmVideo/scriptAsimov.py
@@ -1,12 +1,15 @@
-#Desafio de programa de Entrevista
+print('---------------- Inicio --------------------')
 
-nome = input('Qual o seu nome? ')
-idade = int(input('Qual a sua idade? '))
-tamanho_nome = str(len(nome))
-idade_futura = idade + 5
+resposta1 = input('Você está com fome? (Digite s para sim)')
+if resposta1 == 's':
+    resposta2 = input('Você tem comida? (Digite s para sim)')
+    if resposta2 != 's':
+        print('Vá ao mercado')
+        print('Compre as comidas')
+        print('Volte para casa')
+    print('Prepare sua comida')
+    print('Se alimente')
 
-# Utilização da F STRING para digitar e formar frases é mais prático e deixa o código mais simples.
-# É possivel colocar variaveis apenas usando os colchetes {}, também é possivel fazer operações matematicas e operações do propria python, como tamanho do texto, colocar letras maiusculas.
-print(f'Prazer {nome} eu sou o Gabriel, analista de dados da Mercedez, como posso te ajudar?')
-print(f'Seu nome possui {tamanho_nome} letras')
-print(f'Interessante saber que você tem {idade} eu tenho uma idade um pouco mais abaixo, tenho 25 anos, daqui a 5 anos você terá {idade_futura} anos.')
+
+print ('----------------FIM---------')
+
