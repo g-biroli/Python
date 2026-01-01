5ac8a89411f2d41f5e1f01c9d3afdf9a21ea5ffa
Gabriel Biroli <gabrielbiroli@gmail.com>
2026-01-01 10:55:07 -0300

    Exercicio Login

--- CursoEmVideo/scriptAsimov.py
+++ CursoEmVideo/scriptAsimov.py
@@ -1,15 +1,14 @@
-print('---------------- Inicio --------------------')
+usuario_correto = 'Gabriel'
+senha_correta = 1234
 
-resposta1 = input('Você está com fome? (Digite s para sim)')
-if resposta1 == 's':
-    resposta2 = input('Você tem comida? (Digite s para sim)')
-    if resposta2 != 's':
-        print('Vá ao mercado')
-        print('Compre as comidas')
-        print('Volte para casa')
-    print('Prepare sua comida')
-    print('Se alimente')
+print('Bem vindo, faça o login para continuar')
+usuario_solicitado = input('USUARIO: ')
+senha_solicitada = int(input('SENHA: '))
 
-
-print ('----------------FIM---------')
-
+if usuario_solicitado == usuario_correto:
+    if senha_solicitada == senha_correta:
+        print(f'Login feito com sucesso, seja bem vindo {usuario_solicitado}')
+    else:  
+        print(f'Senha Incorreta')
+else:
+    print(f'Usuário não cadastrado. \nFaça seu cadastrado')
