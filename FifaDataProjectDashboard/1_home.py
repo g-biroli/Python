21f7af384d901e65ad75f11fe4000c51fa84bf9b
Gabriel Biroli <gabrielbiroli@gmail.com>
2026-01-06 22:20:43 -0300

    updates on project

--- FifaDataProjectDashboard/1_home.py
+++ FifaDataProjectDashboard/1_home.py
@@ -1,1 +1,21 @@
 import streamlit as st 
+import webbrowser
+
+# Title
+st.markdown('# FIFA23 OFFICIAL DATASET! ⚽')
+st.sidebar.markdown('Developed by [Gabriel Biroli] (https://www.linkedin.com/in/gabrielbiroli/)')
+
+# Dataset button for sharing with the audience
+btn = st.button('Explore the dataset on kaggle 👌')
+if btn == True:
+    webbrowser.open_new_tab('https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data')
+
+st.markdown(
+    """
+    The football player dataset from 2017 to 2023 provides comprehensive information about professional football players.
+    The dataset contains a wide range of attributes, including player demographics, physical characteristics, game statistics, contract details, and club affiliations.
+    
+    With over **17,000 records**, this dataset offers a valuable resource for football analysts, researchers, and enthusiasts interested in exploring various aspects of the football world, 
+    as it allows for the study of player attributes, performance metrics, market valuation, club analysis, player positioning, and player development over time.
+    """
+    )
