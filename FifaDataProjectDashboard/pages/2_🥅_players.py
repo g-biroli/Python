467418c0a409f79307873f035a8d1f77b5a9e9e5
Gabriel Biroli <gabrielbiroli@gmail.com>
2026-01-07 18:20:13 -0300

    Update 2_🥅_players.py

--- FifaDataProjectDashboard/pages/2_🥅_players.py
+++ FifaDataProjectDashboard/pages/2_🥅_players.py
@@ -1,12 +1,21 @@
 import streamlit as st 
 
 df_data = st.session_state['data']
-df_data
 
 # Filter [Sidebar] to choose the Team and theirs players.
 clubes = df_data['Club'].value_counts().index
 club = st.sidebar.selectbox('Clube', clubes)
 
-df_players = df_data[df_data['Club'] == club]
-players = df_players['Name'].value_counts().index
+df_players = df_data[(df_data['Club'] == club)]
+players = df_players["Name"].value_counts().index
 player = st.sidebar.selectbox('Player', players)
+
+player_stats = df_data[df_data['Name'] == player].iloc[0]
+
+st.title(player_stats['Name'])
+st.markdown(f'**Club:** {player_stats["Club"]}')
+st.markdown(f'**Position:** {player_stats["Position"]}')
+
+# Layout created for specific information
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(f'**Idade** {player_stats["Age"]}')
